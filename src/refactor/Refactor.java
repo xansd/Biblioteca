966b47dc47f4b26b861c51b9193c313250d869c8
xavi <xavi@localhost>
2018-01-16 09:50:22 +0100

    Versión Inicial

--- src/refactor/Refactor.java
+++ src/refactor/Refactor.java
@@ -15,7 +15,7 @@ public class Refactor {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Clientes cl=new Clientes();
     }
     
 }

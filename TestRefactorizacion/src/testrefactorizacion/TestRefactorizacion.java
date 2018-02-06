ab2ad8649fc31785aff2ff8a4d7e9027c4c1c1be
xavi <xavi@localhost>
2018-02-06 09:18:05 +0100

    Cambio de comentariodfdsfs

--- TestRefactorizacion/src/testrefactorizacion/TestRefactorizacion.java
+++ TestRefactorizacion/src/testrefactorizacion/TestRefactorizacion.java
@@ -16,6 +16,7 @@ public class TestRefactorizacion {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        cliente c=new cliente("IES de Rodeira");
     }
     
 }
